5bbfade18ed25269b1e99edb6092cf2e207fa9f2
Gabriel Galera <gabgalera@hotmail.com>
2023-01-09 13:00:20 -0300

    Featured movie

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,6 @@
 import './featured.scss';
 import React from 'react';
+import { InfoOutlined, PlayArrow } from '@material-ui/icons';
 
 const Featured = () => {
   return(
@@ -9,6 +10,24 @@ const Featured = () => {
         src="https://wallpaperaccess.com/full/3084028.jpg" 
         alt="Profile" 
       />
+      <div className="info">
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/TBBT_logo.svg/485px-TBBT_logo.svg.png" 
+          alt="" />
+        <span className='desc'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem odio ea sequi ullam. Nulla quasi non eligendi incidunt necessitatibus velit. Est dolorem nemo, earum exercitationem dicta quod quae error aliquam!
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
