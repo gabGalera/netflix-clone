89d6e0f4acc0a80c5556ec2115ee6764474c36c0
Gabriel Galera <gabgalera@hotmail.com>
2023-01-10 11:36:04 -0300

    Login page começo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './app.scss';
 import Home from "./pages/home/Home";
+import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 
 const App = () => {
-  return <Register />
+  return <Login />
 };
 
 export default App;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,34 @@
+import './login.scss';
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className='logo' 
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" 
+            alt="Netflix logo" />
+        </div>
+        <div className="container">
+          <form action="">
+            <h1>Sign In</h1>
+            <input type="email" placeholder='Email or phone number'/>
+            <input type="password" placeholder='password' />
+            <button className="loginButton">Sign In</button>
+            <span>
+              New to Netflix? <b>Sign up now.</b>
+            </span>
+            <small>
+              This page is protected by Google reCAPTCHA to ensure you're not a 
+              bot. <b>Learn more</b>.
+            </small>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
