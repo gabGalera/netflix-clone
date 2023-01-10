22653daa829346851d27cd7a89a01f99aa546cd4
Gabriel Galera <gabgalera@hotmail.com>
2023-01-10 11:57:36 -0300

    Arruma alguns posicionamentos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import './app.scss';
 import Home from "./pages/home/Home";
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
 
 const App = () => {
-  return <Login />
+  return <Home />
 };
 
 export default App;
