6a175d461a824529eb3172fc92e3ff0389681ded
Gabriel Galera <gabgalera@hotmail.com>
2023-01-10 11:25:34 -0300

    Register page completa

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,22 @@
 import './register.scss';
 import React from "react";
+import { useState } from 'react';
+import { useRef } from 'react';
 
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  
+  const handleStart = () => {
+    setEmail(emailRef.current.value)
+  }
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value)
+  }
+
   return (
     <div className='register'>
       <div className="top">
@@ -18,10 +33,29 @@ const Register = () => {
           <p>
             Ready to watch? Enter your email to create or restart your membership.
           </p>
-          <div className="input">
-            <input type="email" placeholder='email address'/>
-            <button className="registerButton">Get Started</button>
-          </div>
+          {
+            !email ? (
+              <div className="input">
+                <input type="email" placeholder='email address' ref={emailRef}/>
+                <button 
+                  className="registerButton" 
+                  onClick={handleStart}
+                >
+                  Get Started
+                </button>
+              </div>
+            ) : (
+              <form className="input">
+                <input type="password" placeholder='password' ref={passwordRef}/>
+                <button 
+                  className="registerButton" 
+                  onClick={handleFinish}
+                >
+                  Start
+                </button>
+              </form>
+            )
+          }
         </div>
       </div>
     </div>
