d7d4656694475e3de1d22fe3a2f412b5375d0227
Gabriel Galera <gabgalera@hotmail.com>
2023-01-10 09:04:44 -0300

    Termina as lists e começa o hover effect

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,19 +1,24 @@
 import './list.scss';
-import React from "react";
+import React, { useState } from "react";
 import ListItem from '../listItem/ListItem'
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons';
 import { useRef } from 'react';
 
 const List = () => {
+  const [ isMoved, setIsMoved ] = useState(false);
+  const [ slideNumber, setSlideNumber ] = useState(0);
 
   const listRef = useRef();
 
   const handleClick = (direction) => {
+    setIsMoved(true)
     let distance = listRef.current.getBoundingClientRect().x - 50
-    if(direction === 'left') {
+    if(direction === 'left' && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1)
       listRef.current.style.transform = `translateX(${230 + distance}px)`
     }
-    if(direction === 'right') {
+    if(direction === 'right' && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1)
       listRef.current.style.transform = `translateX(${-230 + distance}px)`
     }
   }
@@ -24,10 +29,10 @@ const List = () => {
       <div className="wrapper">
         <ArrowBackIosOutlined 
           className='sliderArrow left'
-          onClick={() => handleClick('left')}  
+          onClick={() => handleClick('left')}
+          style={{display: !isMoved && 'none' }}  
         />
         <div className="container" ref={ listRef }>
-          <ListItem />
           <ListItem />
           <ListItem />
           <ListItem />
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,10 +1,31 @@
 import './listItem.scss';
 import React from "react";
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from '@material-ui/icons';
 
 const listItem = () => {
   return(
     <div className="listItem" >
-      item
+      <img 
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Tjj3ylZ81ZVhaCHOHesA1yfDO73HUMcTog&usqp=CAU" 
+        alt="Game of Thrones logo"
+      />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownAltOutlined />
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 mins</span>
+          <span className='limit'>+16</span>
+          <span>1999</span>
+        </div>
+        <div className="desc">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem facilis quam nostrum fugit recusandae facere, minus hic voluptatum totam veritatis repellendus exercitationem saepe dolores soluta delectus pariatur perspiciatis! Soluta, aliquid.
+        </div>
+        <div className="genre">Action</div>
+      </div>
     </div>
   )
 }
