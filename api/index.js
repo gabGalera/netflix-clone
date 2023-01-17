cf2fbbdb999cc0fda17a03b8f0b91bd30e77312e
Gabriel Galera <gabgalera@hotmail.com>
2023-01-17 13:44:12 -0300

    Configura o MongoDB na cloud AWS

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL,
+  { useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+  })
+  .then(() => console.log("DB connection Successfull!"))
+  .catch((err) => console.log(err))
 
 app.listen(8800, () => {
   console.log('Bakend server is running!')
