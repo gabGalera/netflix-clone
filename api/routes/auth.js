4cc49115ae3ad71bd9173d4b6df91b9d28b279f9
Gabriel Galera <gabgalera@hotmail.com>
2023-01-19 15:18:20 -0300

    Configura o JWT

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require('jsonwebtoken');
 
 // REGISTER
 router.post("/register", async (req, res) => {
@@ -30,7 +31,17 @@ router.post("/login", async (req, res) => {
     originalPassword !== req.body.password &&
       res.status(401).json("Wrong password or username!");
 
-      res.status(200).json(user);
+    const accessToken = jwt.sign(
+      { 
+        id: user._id, isAdmin: user.isAdmin 
+      }, 
+      process.env.SECRET_KEY, 
+      { expiresIn: "5d" }
+      )
+
+    const { password, ...info } = user._doc;
+
+    res.status(200).json({ ...info, accessToken });
 
   } catch(err) {
     console.error(err)
